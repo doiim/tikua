5bfff8f984c1fa1850f22f4f76a88246de7cc08e
Mateus <94379499+dalpii@users.noreply.github.com>
2024-04-24 17:35:25 +0000

    Update styling and dark mode support in Vue app

--- examples/tikua-front-vue/src/App.vue
+++ examples/tikua-front-vue/src/App.vue
@@ -22,6 +22,7 @@ const walletAddress = ref<Address>();
 const provider = ref()
 const dragonIdInput = ref('')
 const message = ref<any>({})
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const connect = async () => {
   if (!window.ethereum) throw Error('MetaMask not found')
@@ -111,61 +112,61 @@ const attackDragon = async () => {
 </script>
 
 <template>
-  <div class="text-stone-600">
-    <img src="./assets/logo.png" width=200 height=200 />
+  <div class="text-stone-600 dark:text-stone-300  overflow-hidden">
+    <img src="./assets/logo.png" width=200 height=200 class="mx-auto" />
     <div class="text-center">
       <h1 class="text-5xl font-semibold">Tikua</h1>
       <p>an isomorphic Cartesi SDK</p>
     </div>
 
 
-    <div class="my-10">
+    <div class="my-10 text-center">
       <button v-if="!walletAddress" @click="connect"
-        class="bg-stone-100 rounded-lg p-2 px-4 font-semibold text-lg hover:outline hover:outline-1 hover:outline-blue-600">Connect
+        class="bg-stone-100 dark:bg-stone-800 rounded-lg p-2 px-4 font-semibold text-lg hover:outline hover:outline-1 hover:outline-blue-600">Connect
         Wallet</button>
       <p v-else>Wallet: <span class="text-orange-400">{{ walletAddress }}</span></p>
     </div>
 
-    <p class="my-2">
+    <p class="my-2 text-center">
       After connect your wallet successfully. Fill the form above.
     </p>
 
-    <div class="bg-stone-50 rounded-lg p-4 flex flex-col gap-2 border border-stone-100 w-[600px] my-2">
-      <h1 class="uppercase text-sm font-semibold text-stone-500">Hero</h1>
+    <div
+      class="bg-stone-50 dark:bg-stone-700 rounded-lg p-4 flex flex-col gap-2 border border-stone-100 dark:border-stone-800 w-[600px] my-2">
+      <h1 class="uppercase text-sm font-semibold text-stone-500 dark:text-stone-300">Hero</h1>
       <div class="flex gap-4">
         <button :disabled="!walletAddress" @click="checkLife"
-          class="bg-stone-100 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Check
+          class="bg-stone-100 dark:bg-stone-800 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Check
           Health</button>
         <button :disabled="!walletAddress" @click="drinkPotion"
-          class="bg-stone-100 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Drink
+          class="bg-stone-100 dark:bg-stone-800 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Drink
           Potion</button>
       </div>
     </div>
 
 
-    <form class="bg-stone-50 rounded-lg p-4 flex flex-col gap-2 border border-stone-100 w-[600px] my-2">
-      <h1 class="uppercase text-sm font-semibold text-stone-500">Dragon</h1>
+    <form
+      class="bg-stone-50 dark:bg-stone-700 rounded-lg p-4 flex flex-col gap-2 border border-stone-100 dark:border-stone-800 w-[600px] my-2">
+      <h1 class="uppercase text-sm font-semibold text-stone-500 dark:text-stone-300">Dragon</h1>
       <div>
         <button @click.prevent="dragonsList"
-          class="bg-stone-100 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">List
+          class="bg-stone-100 dark:bg-stone-800 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">List
           All Dragons</button>
       </div>
       <div class="flex gap-4">
         <input v-model="dragonIdInput" placeholder="Enter a dragon ID"
-          class="bg-white rounded-lg p-2 px-4  text-md focus:outline focus:outline-1 focus:outline-blue-600 w-40" />
+          class="bg-white dark:bg-stone-800 rounded-lg p-2 px-4  text-md focus:outline focus:outline-1 focus:outline-blue-600 w-40" />
 
         <button @click.prevent="checkDragon" v-if="dragonIdInput"
-          class="bg-stone-100 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Check
+          class="bg-stone-100 dark:bg-stone-800 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Check
           Health</button>
         <button @click.prevent="attackDragon" :disabled="!walletAddress" v-if="dragonIdInput"
-          class="bg-stone-100 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Attack</button>
-
+          class="bg-stone-100 dark:bg-stone-800 rounded-lg p-2 px-4  text-md hover:outline hover:outline-1 hover:outline-blue-600">Attack</button>
       </div>
     </form>
 
-    <div class="self-start my-2">
-      <VueJsonPretty :data="message" :show-icon="true" theme="dark" />
-    </div>
+    <VueJsonPretty :data="message" :show-icon="true" :theme="isDarkMode ? 'dark' : 'light'"
+      class="bg-stone-50 dark:bg-stone-700 rounded-lg p-4" />
 
   </div>
 </template>
