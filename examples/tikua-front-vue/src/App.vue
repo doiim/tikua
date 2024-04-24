4f0f400407fbd22d986a3c096a8c38dce6963253
Mateus <94379499+dalpii@users.noreply.github.com>
2024-04-24 13:37:39 +0000

    Import styling and basic structure from React example

--- examples/tikua-front-vue/src/App.vue
+++ examples/tikua-front-vue/src/App.vue
@@ -1,20 +1,66 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+// TODO: didn't work as expected
+// import VueJsonPretty from 'vue-json-pretty'
+
+const connectedWalletAddress = ref('');
+const connectedProvider = ref()
+const dragonIdInput = ref<number>()
+const message = ref({root: {hello: 666}})
+
+const connect = async () => {
+  if (!window.ethereum) throw Error('MetaMask not found')
+  const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  if (!account) throw Error('MetaMask reject')
+  connectedWalletAddress.value = account
+  connectedProvider.value = window.ethereum
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main">
+    <img src="./assets/logo.png" width=200 height=200 />
+    <h1>Tikua</h1>
+    <p>an isomorphic Cartesi SDK</p>
+    <div class="card">
+      <button v-if="!connectedWalletAddress" @click="connect">Connect Wallet</button>
+      <p v-else>Wallet: <span class='orange'>{{ connectedWalletAddress }}</span></p>
+
+      <p>
+        After connect your wallet successfully. Fill the form above.
+      </p>
+
+      <div class='heroCommands'>
+          <button :disabled="!connectedWalletAddress" >Drink Potion</button>
+          <button :disabled="!connectedWalletAddress">Check Hero Status</button>
+          <button :disabled="!connectedWalletAddress">List Dragons</button>
+      </div>
+      <form>
+        <label>
+          <span>Dragon ID</span>
+          <input type="number" v-model="dragonIdInput" />
+        </label>
+        <input type="submit" value="Dragon Status"  />
+        <input type="submit" value="Attack Dragon"  />
+      </form>
+
+      <div class='message' >
+        {{ message }}
+        
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.main {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -24,7 +70,48 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.logo.react:hover {
+  filter: drop-shadow(0 0 2em #61dafbaa);
 }
+
+@keyframes logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  a:nth-of-type(2) .logo {
+    animation: logo-spin infinite 20s linear;
+  }
+}
+
+.card {
+  padding: 2em;
+}
+
+.orange {
+  color: orange;
+}
+
+form,
+.heroCommands {
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  margin: 20px;
+}
+
+form label {
+  display: flex;
+  gap: 20px;
+}
+
+.message {
+  text-align: start;
+}
+
 </style>
